25e38a0763f5540eceb610144d3e4958bb438618
Fariha Noor <fariha.git@gmail.com>
2021-02-03 19:48:06 +0600

    Create Grasshopper - Function syntax debugging.py

---
+++ Python/Grasshopper - Function syntax debugging.py
@@ -0,0 +1,13 @@
+# A student was working on a function and made some syntax mistakes while coding. 
+# Help them find their mistakes and fix them.
+
+"""
+Intially:
+def main [verb, noun]
+return verb + noun
+"""
+
+# Corrected:
+
+def main(verb, noun):
+    return verb + noun
